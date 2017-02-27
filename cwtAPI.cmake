0f1c1d3689fa1c5abe69af37230ed4d10f1684a0
jeremy viau-trudel <Blanc-Sablon+jeremy@Blanc-Sablon.cidco.ca>
2017-02-27 09:40:01 -0500

    .

---
+++ cwtAPI.cmake
@@ -0,0 +1,26 @@
+set(CWT_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR})
+LIST(APPEND CMAKE_MODULE_PATH ${CWT_SOURCE_DIR}/cmake)
+
+include(cwtConfig)
+
+
+macro(cwt_add_site)
+
+endmacro()
+
+macro(cwt_add_content)
+
+endmacro()
+
+macro(add_images)
+
+endmacro()
+
+macro(cwt_add_metadata)
+
+endmacro()
+
+
+macro(get_layout)
+
+endmacro()
